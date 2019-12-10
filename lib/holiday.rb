44747a29eec65c1a23fe4b0bd9bdf519f0f141e9
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 11:29:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,15 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holidy| 
+    puts season
+    holiday.each do |holiday, supplies|
+      print holiday + ": "
+      supplies.each do |item|
+        print item
+      end
+    end
+  end
 
 end
 
