4c52bec971f30f259dd81a952b33c79ebf651e92
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 11:50:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday| 
-    puts season.to_s.gsub("_", " ").capitalize + ":"
+    puts season.to_s.gsub(/_/, " ").capitalize + ":"
     holiday.each do |holiday1, supplies|
       phrase = "  "+ holiday1.to_s.capitalize + ": "
       phrase += supplies.join(", ")
